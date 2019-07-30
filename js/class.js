59e5b17973351f9093565bf594fb069fde21d687
Sattar Amrahov <sattar@code.local>
2019-07-30 12:10:51 +0400

    files added

---
+++ js/class.js
@@ -0,0 +1,12 @@
+"use strict";
+
+class Student {
+    
+}
+
+let s = new Student("Fuad", "Tagiyev");
+
+console.log("s variable created");
+ 
+
+console.log(s);
---
+++ js/script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// x = 7;
+
+
+
+
+
+// try {
+//     alert("Test alert");
+
+// } catch(e){
+//     console.log(e);
+// } finally {
+//     console.log("this is finally");
+    
+// }
+
+
+// document.querySelector("#demo").innerHTML = "Lorem ipsum";
+
+// Test();
+myNewName = "test";
+// function Test(){
+//     name = "Misir";
+// }
+
+// console.log(name);
